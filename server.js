d9682cf657fb2e38d4bb78645286e351ae11db03
Gabriel Caires <gcaires@thoughtworks.com>
2017-12-05 19:35:50 -0300

    Add start script

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const Koa = require('koa')
 
 const port = () => process.env.PORT || 3000
+const env = () => process.env.NODE_ENV
 const app = new Koa()
     .use(require('koa-json')())
     .use(require('koa-logger')())
     .use(require('./src/routes'))
     .listen(port())
     .on('listening',
-        () => console.log(`App Running on port ${port()}`))
+        () => console.log(`App Running on port ${port()} with NODE_ENV=${env()}`))
 
 module.exports = app
